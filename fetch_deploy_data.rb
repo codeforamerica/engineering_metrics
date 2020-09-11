b00b9f8bd0500e52b65f36e21aa78c5f3c72b5bc
Laura Kogler <lkogler@codeforamerica.org>
2020-09-10 18:55:12 -0700

    Add Git Tags reader for GCF deploys

--- fetch_deploy_data.rb
+++ fetch_deploy_data.rb
@@ -3,9 +3,15 @@
 require 'rest-client'
 require 'json'
 require_relative 'circle_ci_client'
+require_relative 'git_tags_reader'
+require_relative 'secrets'
 
 def fetch_gyr(start_date, end_date)
   CircleCiClient.get_deploys(start_date, end_date, 'gh/codeforamerica/vita-min', 'deploy-release-branch-to-production', 'release')
+end
+
+def fetch_gcf(start_date, end_date)
+  GitTagsReader.get_deploys(start_date, end_date, GCF_REPO_PATH, 'deploy-production-', '%Y-%m-%d_%H-%M-%S')
 end
 
 unless ARGV.count == 1
@@ -17,12 +23,13 @@ end_date = DateTime.strptime(date_input, '%m/%d/%Y')
 start_date = end_date - 7
 
 
-# gcf_deploys = fetch_gcf
+gcf_deploys = fetch_gcf start_date, end_date
 gyr_deploys = fetch_gyr start_date, end_date
 # cmr_deploys = fetch_cmr
 # shiba_deploys = fetch_shiba
 
-puts "#{gyr_deploys} successful deploys"
+puts "GCF: #{gcf_deploys} deploys"
+puts "GYR: #{gyr_deploys} deploys"
 
 
 
---
+++ git_tags_reader.rb
@@ -0,0 +1,15 @@
+require 'git'
+
+class GitTagsReader
+  def self.get_deploys(start_date, end_date, repo_path, tag_prefix, tag_date_format)
+    Dir.chdir(repo_path) do
+      system "git pull"
+      deploy_tags = `git tag -l #{tag_prefix}*`
+      deploy_dates = deploy_tags
+                       .split("\n")
+                       .map { |t| DateTime.strptime(t.delete_prefix(tag_prefix), tag_date_format)}
+                       .select { |d| d.between?(start_date, end_date) }
+      deploy_dates.count
+    end
+  end
+end
